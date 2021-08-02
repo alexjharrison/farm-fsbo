4e3896553a450449d739b0125cdcdef56c44fc9d
Alex Harrison <alexjharrison@gmail.com>
2021-08-02 18:09:21 -0400

    strip content

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,36 +5,6 @@
         <b-navbar toggleable="md" type="dark" variant="secondary">
           <div class="container-fluid mw-1440 px-2">
             <b-navbar-brand :to="{ path: '/' }">Milton PA Farm</b-navbar-brand>
-            <b-navbar-toggle target="nav-collapse" />
-            <b-collapse is-nav id="nav-collapse">
-              <b-navbar-nav class="ml-auto">
-                <b-nav-item
-                  :active="$route.path === '/property'"
-                  :to="{ path: '/property' }"
-                  >The Property</b-nav-item
-                >
-                <b-nav-item
-                  :active="$route.path === '/house'"
-                  :to="{ path: '/house' }"
-                  >House Tour</b-nav-item
-                >
-                <b-nav-item
-                  :active="$route.path === '/farm'"
-                  :to="{ path: '/farm' }"
-                  >The Farm</b-nav-item
-                >
-                <b-nav-item
-                  :active="$route.path === '/history'"
-                  :to="{ path: '/history' }"
-                  >The History</b-nav-item
-                >
-                <b-nav-item
-                  :active="$route.path === '/contact'"
-                  :to="{ path: '/contact' }"
-                  >Contact</b-nav-item
-                >
-              </b-navbar-nav>
-            </b-collapse>
           </div>
         </b-navbar>
       </header>
@@ -49,7 +19,9 @@
 <script>
 export default {
   components: {},
-  methods: {}
+  beforeMount() {
+    this.$router.push('/')
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -4,82 +4,8 @@
       <div
         class="mt-auto mb-5 container d-flex flex-column align-items-end text-right"
       >
-        <h1 class="flex-shrink-1">Central PA Farmette for Sale</h1>
-        <h1>$500,000</h1>
-      </div>
-    </div>
-    <div class="text-center m-5">
-      <em>
-        <h1>For Sale</h1>
-      </em>
-      <h2 class="mb-4">~23 Acre Picturesque Farmette near Milton, PA</h2>
-
-      <h4>
-        Gently Rolling Hills Overlooking the Beautiful Milton 18 Hole Golf
-        Course
-      </h4>
-      <h4 class="mb-4">Additional Acreage Available Separately</h4>
-      <h4>
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://goo.gl/maps/W2Rv29ryLkvzHWeq9"
-          >704 Klinetob Rd, Milton, PA 17847</a
-        >
-      </h4>
-      <b-button :to="{ path: '/property' }" size="lg" variant="dark" class="m-3"
-        >Property Details</b-button
-      >
-      <b-button
-        :to="{ path: '/contact' }"
-        size="lg"
-        variant="outline-dark"
-        class="m-3"
-        >Contact Us</b-button
-      >
-    </div>
-    <div class="custom-container mb-5">
-      <div class="row text-center mx-2">
-        <div class="col-md-4 my-2">
-          <b-card
-            title="The Farm"
-            img-top
-            img-alt="farmhouse"
-            img-src="~assets/images/home/farm-thumb.jpg"
-          >
-            <b-card-text>See what the farm has to offer</b-card-text>
-            <b-button :to="{ path: '/farm' }" variant="outline-primary"
-              >Check it out ➚</b-button
-            >
-          </b-card>
-        </div>
-        <div class="col-md-4 my-2">
-          <b-card
-            title="The House"
-            img-alt="house"
-            img-top
-            img-src="~assets/images/home/house-thumb.jpg"
-          >
-            <b-card-text>Explore the house in a virtual tour</b-card-text>
-            <b-button :to="{ path: '/house' }" variant="outline-primary"
-              >Check it out ➚</b-button
-            >
-          </b-card>
-        </div>
-
-        <div class="col-md-4 my-2">
-          <b-card
-            title="The Property"
-            img-top
-            img-alt="farmhouse being built"
-            img-src="~assets/images/home/history-thumb.jpg"
-          >
-            <b-card-text>Learn the history of the farm</b-card-text>
-            <b-button :to="{ path: '/history' }" variant="outline-primary"
-              >Check it out ➚</b-button
-            >
-          </b-card>
-        </div>
+        <h1 class="flex-shrink-1">This property is off the market</h1>
+        <p class="h3">Thanks for your interest</p>
       </div>
     </div>
   </main>
